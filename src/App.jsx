a635b76cd503ce25c109c4b6976696ce5569c961
Akim Ospan <ospanakim05@gmail.com>
2024-09-04 17:27:07 +0500

    React Life Cycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <div>
-      Hello world
-    </div>
-  )
+  constructor(){
+    super()
+    console.log('1')
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleButton = ()=>{
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+
+  componentDidUpdate(){
+    console.log('5')
+  }
+
+  componentDidMount(){
+    console.log('4')
+  }
+
+
+  render() {
+    console.log('2')
+
+    return (
+      <div>   
+        <h1>Hello world</h1>
+    {console.log('3')}
+
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleButton}>Increase Count</button>
+        {/* <button onClick={this.handleButton}>Descrease Count</button> */}
+      </div>
+    );
+  }
 }
 
 export default App
